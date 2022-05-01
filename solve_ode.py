6adade78b87af337f682188037211ef806250374
lee_K <nj19257@bristol.ac.uk>
2022-05-01 01:05:19 +0100

    replacing solve_ode by solve_ode_final.py as solve_ode_finial.py is the final version

--- solve_ode.py
+++ solve_ode.py
@@ -2,6 +2,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from arbitary_differential_equation import *
 import time
 
 def euler_step(f, x, t, h,*args): #work
@@ -12,6 +13,7 @@ def euler_step(f, x, t, h,*args): #work
             x_current : the current value of x
             t_current : the current timestep
             h : interval between step (step size) (assumed to be constant for the sake of simplicity) is then given by t_{n+1} = h + t_n
+            args : Any extra arguments .
 
 
     :return:
@@ -29,11 +31,12 @@ def RK4_step(f, x, t, h,*args): #work
             x_current : the current value of x
             t_current : the current timestep
             h : interval between step (step size) (assumed to be constant for the sake of simplicity) is then given by t_{n+1} = h + t_n
-
+            args : Any extra arguments .
 
     :return:
-    [ x_{n+1} , t_{n+1} ] or [x_new , t_new] , which it the x and t for the next step
+    [ x_{n+1} , t_{n+1} ] or [x_new , t_new] , which x_new and t_new are the value of x and t for the next timestep
     """
+
     k1 = f(x, t,*args)
     k2 = f(x + h * (k1/2) , t + h / 2, *args)
     k3 = f(x + h * (k2/2), t + h / 2, *args)
@@ -52,9 +55,10 @@ def solve_to( f ,x , t , t1 , deltat_max , Method,*args): # work
             t1 : the value of next timestep based on the current timestep
             deltat_max : The maximum timestep allowed
             Method : Which method to solve the ODE
+            args : Any extra arguments .
     :return:
+    x : the x value for the target timestep
     """
-
     Method=str.lower(Method) #to ignore error cause by upper_case input
     #Determine which method to be used
     if Method == 'euler':
@@ -68,19 +72,24 @@ def solve_to( f ,x , t , t1 , deltat_max , Method,*args): # work
         x, t = method(f, x, t, t1 - t, *args)  # bridges gap between last step and t2 using step size t2-t_prev_step
     return x
 
-def solve_ode( f , x , t_0 , t_end , deltat_max , Method  , *args,n=5, plot=False , timer=False):
+def solve_ode( f , x ,  t_end ,  Method  , *args, deltat_max =1e-3 ,n=5 ,t_0=0 , plot=False , timer=False):
     """
-    single Euler step function
+    The function that use solve_to by a first-order numerical procedure for ODE (such as forward euler and 4th-order Runge-Kutta method)
+    to generate a series of numerical solution estimates x1,x2,x3,...
         Parameter:
             f(function) = function used ( need to define more detail)
             x_current : the current value of x
-            t_0 : the initial timestep
+            t_0 : the initial timestep (default start time is 0 )
             t_end : the target timestep
-            n = number of step to reach the target timestep(the default timesteps is 5)
+            Method : Which method to use
+            deltat_max : The maximum timestep allowed (default value is 1e-3 )
+            n : number of step to reach the target timestep(the default timesteps is 5)
+            args : Any extra arguments .
 
     :return:
+    x_list : the values of x corresponding to t(time)
+    t : the time of x in x_list
     """
-
     if timer == True:
         start = time.time()
     t = np.linspace(t_0, t_end, n )
@@ -95,36 +104,57 @@ def solve_ode( f , x , t_0 , t_end , deltat_max , Method  , *args,n=5, plot=Fals
     if timer == True:
         end = time.time()
         print("Time taken for %s = %f sec" % (Method, end - start))
+    #plot figure of x against y
     if plot == 'plot_x_y':
         plot_x_y(x_list, Method)
+    # plot figure of x against t
     elif plot == 'plot_x_t':
         plot_x_t(x_list[:,0],t, Method, 'x')
+    # plot figure of y against t
     elif plot == 'plot_y_t':
         plot_y_t(x_list[:,1],t, Method, 'y')
-
+    elif plot == False:
+        # No changes are made
+        pass
+    else:
+        print('The input string for plot parameter is incorrect')
     return x_list ,t
 
 
 
 
 
-def error_depend_deltat_t( f ,x , t_0 , t_end ,h, Method , *args ,**kwargs): # plot=False , timer = False):
-    if kwargs['timer'] == True:
+def error_depend_deltat_t( f ,x  , t_end ,h, Method , *args , plot=False , timer=False):
+    """
+    Function that find out the error depending on a range of delta t
+        Parameter:
+            f(function) = function used ( need to define more detail)
+            x_current : the current value of x
+            t_end : the target timestep
+            h : The list of values for maximum timesteps allowed for testing
+            Method : Which method to use (Forward Euler or RK4 Method)
+            n : number of step to reach the target timestep(the default timesteps is 5)
+
+
+    :return:
+    list_error =
+    """
+    if timer == True:
         start = time.time()
 
     list_error = np.zeros(len(h))
     for i in range(len(h)):
-        x_list ,t =solve_ode(f, x, t_0 , t_end , h[i], Method,*args)
+        x_list ,t =solve_ode(f, x , t_end , Method,*args , deltat_max=h[i])
         if f == dxdt_equal_x:
             abs_error=find_error_dxdt_result(x_list[-1], t_end)
 
         list_error[i]=abs_error
         #print(x_list[-1])
-    if kwargs['timer'] == True:
+    if timer == True:
         end = time.time()
         print("Time taken for %s = %f sec" % (Method, end - start))
     #Therefore , the plot function wont take into account of the method
-    if kwargs['plot'] == True:
+    if plot == True:
         plot_loglog(h,list_error,Method)
     return list_error
 
@@ -153,9 +183,10 @@ def plot_x_y( x_list ,Method):
     plt.show()
 
 def plot_x_t( x_list ,t,Method ,xlabel):
+
     plt.plot(t, x_list, label=Method)
-    plt.xlabel(xlabel)
-    plt.ylabel('t')
+    plt.xlabel('t')
+    plt.ylabel(xlabel)
     plt.show()
 
 def same_error(list_error_1,list_error_2 , h , tol=1e-5):
@@ -172,43 +203,19 @@ def same_error(list_error_1,list_error_2 , h , tol=1e-5):
         print('no step-sizes that give same error in the two method')
     return output
 
-def dxdt_equal_x(x,t):
-    dxdt= x
-    return dxdt
 
-def d2xdt2_equals_minus_x( r , t):
-    """
-    Function defining system of  ODEs dxdt = y, dy/dt = -x
-    :param t: t value
-    :param r: array [x, y]
-    :return: returns value of dxdt and dy/dt at (t,u)
-    """
-    dxdt = r[1]
-    dydt = -r[0]
-
-    return np.array([dxdt, dydt])
-def d2xdt2_equals_minus_x_true(t):
-    """
-    The general solution for 2nd order ODE d2xdt2_equals_minus_x()
-    :return: Return the true value of x and y at a given time
-    """
-    x = np.sin(t) + np.cos(t)
-    y = np.cos(t) - np.sin(t)
-    return np.array([x, y])
-#list_h=np.logspace(-1 , -6 , 100 ) #Therefore, h will be always smaller than the default timestep
-#list_error_rk4 = error_depend_deltat_t(dxdt_equal_x,1 ,0 ,1 ,list_h , 'rk4', plot=True , timer = True)
-#list_error_euler = error_depend_deltat_t(dxdt_equal_x,1 ,0 ,1 ,list_h ,  'euler', plot=True , timer = True)
-#plt.show()
-#print(same_error(list_error_rk4,list_error_euler , list_h))
 """
 This section is used to answer Q3
 """
 def main():
-    #list_h=np.logspace(-1 , -6 , 100 ) #Therefore, h will be always smaller than the default timestep
-    #list_error_rk4 = error_depend_deltat_t(dxdt_equal_x, 1, 0, 1, list_h, 'rk4', plot=True, timer=True)
+    list_h=np.logspace(-1 , -6 , 100 ) #Therefore, h will be always smaller than the default timestep
+    list_error_rk4 = error_depend_deltat_t(dxdt_equal_x,1 ,1 ,list_h , 'rk4', plot=True , timer = True)
+    list_error_euler = error_depend_deltat_t(dxdt_equal_x,1  ,1 ,list_h ,  'euler', plot=True , timer = True)
+    plt.show()
+    print(same_error(list_error_rk4,list_error_euler , list_h))
     n = 500
-    euler_output,t=solve_ode(d2xdt2_equals_minus_x,[1,1] ,0 ,100 , 0.001,  'euler' , n=n , timer=True)
-    rk4_output,t=solve_ode(d2xdt2_equals_minus_x,[1,1] ,0 ,100 , 0.001,  'rk4' , n=n  ,timer=True )
+    euler_output,t=solve_ode(d2xdt2_equals_minus_x,[1,1]  ,100 ,   'euler' , n=n , timer=True)
+    rk4_output,t=solve_ode(d2xdt2_equals_minus_x,[1,1]  ,100 ,   'rk4' , n=n  ,timer=True )
     true_answer=d2xdt2_equals_minus_x_true(t)
     #plot_xlabel_ylabel('t', x, x , y ,Method)
     plt.subplot(2, 1, 1)
